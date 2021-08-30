d9b6a5ce5506794a1424d3ee3914741fe602c8e6
Jônathas Gouveia <jonathas_gouv@hotmail.com>
2021-08-30 03:05:45 -0300

    removes useNewUrlParser option

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-mongoose.connect(process.env.MONGODBURL, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODBURL)
 mongoose.Promise = global.Promise
 
 export default mongoose
